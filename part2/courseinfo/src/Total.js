db305be7e8cd78f8d054b9bbcb59cefb2084d18e
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-06-11 14:49:16 +0200

    Removed console.log

--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Total = ({exercises}) => {
-    console.log(exercises);
     return (
         <p><b>Total number of exercises</b> {exercises.reduce((acc,el) => acc+el, 0)}</p>
     )
