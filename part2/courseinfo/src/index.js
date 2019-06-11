5086eb4cea0cac744d87d82268175298fd6cf78a
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-06-11 14:50:32 +0200

    Accepts arbitrary # of courses

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -5,8 +5,10 @@ import Course from './Course';
 
 
 const App = () => {
-    const course = {
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -22,16 +24,38 @@ const App = () => {
           name: 'State of a component',
           exercises: 14,
           id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
         }
       ]
     }
-  
-    return (
-      <div>
-        <Course course={course} />
-      </div>
-    )
-  }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => <Course course={course} key={course.id}/>)}
+    </div>
+  )
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
